1043a5830bf3c7eb48298b90e9e3cae5247d3986
maciej <maciej.kalenicz@gmail.com>
2017-11-28 15:12:31 +0100

    Added phone intent

--- app/src/main/java/com/kalenicz/maciej/singlescreen/MainActivity.java
+++ app/src/main/java/com/kalenicz/maciej/singlescreen/MainActivity.java
@@ -1,7 +1,11 @@
 package com.kalenicz.maciej.singlescreen;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setCompanyDetails();
+    }
+
+    private void setCompanyDetails(){
+        TextView businessName = (TextView) findViewById(R.id.business_name);
+        businessName.setText("Wroclove Apartment");
+        TextView phoneNumber = (TextView) findViewById(R.id.phone_number);
+        phoneNumber.setText("+48602393632");
+
+    }
+
+    public void dialPhoneNumber(View view) {
+        TextView phoneNumber = (TextView) findViewById(R.id.phone_number);
+       String number = phoneNumber.getText().toString();
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse("tel:" + number));
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 }
